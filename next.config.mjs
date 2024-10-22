29a17428c3c5ddf78e0d0455f44a474ead261a93
NanaManBo <kimbosung22@naver.com>
2024-10-22 17:38:02 +0900

    폰트 추가 및 개발 모드 시 pwa 오류 수정

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import nextPWA from 'next-pwa';
 const withPWA = nextPWA({
   dest: 'public',
   register: true,
+  disable: process.env.NODE_ENV === 'development', // 개발 모드에서는 PWA 비활성화
   skipWaiting: true,
 });
 
