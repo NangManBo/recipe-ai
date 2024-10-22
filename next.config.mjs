adc587fa3422ac48b06793f80bbf040e389ab814
NanaManBo <kimbosung22@naver.com>
2024-10-22 18:58:40 +0900

    🐛 layout 다시 수정

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,10 @@ const withPWA = nextPWA({
   register: true,
   disable: process.env.NODE_ENV === 'development', // 개발 모드에서는 PWA 비활성화
   skipWaiting: true,
+  buildExcludes: [
+    /app-build-manifest\.json$/,
+    /_buildManifest\.js$/,
+  ],
 });
 
 export default withPWA({
