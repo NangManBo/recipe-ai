892fc5bf0a3c86879b635f62a3308263504d8915
NanaManBo <kimbosung22@naver.com>
2024-10-26 06:07:00 +0900

    pwa 환경설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,67 @@
-import nextPWA from 'next-pwa';
+import withPWA from 'next-pwa';
 
-const withPWA = nextPWA({
+// NODE_ENV가 'production'일 때만 PWA 활성화
+const isProd = process.env.NODE_ENV === 'production';
+
+const pwaConfig = {
   dest: 'public',
   register: true,
-  disable: process.env.NODE_ENV === 'development', // 개발 모드에서는 PWA 비활성화
   skipWaiting: true,
+  disable: !isProd, // 개발 모드에서는 PWA 비활성화
   buildExcludes: [
     /app-build-manifest\.json$/,
     /_buildManifest\.js$/,
   ],
-});
+};
 
-export default withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
+  swcMinify: true,
+  output: 'standalone', // 독립 실행을 위한 설정
+
+  // 외부 이미지 도메인 허용 설정 추가
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http', // HTTP 이미지 허용
+        hostname: 'img1.kakaocdn.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 't1.kakaocdn.net',
+        pathname: '/**',
+      },
+    ],
+  },
+
+  // Webpack 설정 추가
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = config.externals || [];
+      config.externals.push({
+        'rc-util': 'commonjs rc-util',
+      });
+    }
+
+    config.module.rules.push({
+      test: /\.(js|jsx|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['next/babel'],
+          plugins: [
+            '@babel/plugin-transform-private-methods',
+            '@babel/plugin-transform-runtime',
+          ],
+        },
+      },
+    });
+
+    return config;
+  },
+};
+
+// PWA 설정과 Next.js 설정을 통합하여 export
+export default withPWA(pwaConfig)(nextConfig);
